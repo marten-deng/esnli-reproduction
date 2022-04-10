85bf17aa626ee567c305e61ffc6b9b77fa5b99b9
Marten <dengmarten03@gmail.com>
2022-04-10 10:40:26 +1000

    Fixing evaluation

--- seq2seq/launch_eval.py
+++ seq2seq/launch_eval.py
@@ -58,8 +58,8 @@ torch.cuda.manual_seed(params.seed)
 torch.backends.cudnn.deterministic = params.cudnn_deterministic
 
 
-copy2('./launch_eval.py', eval_params.directory)
-copy2('./eval_sent_embeddings_labels_in_expl.py', eval_params.directory)
+copy2('seq2seq/launch_eval.py', eval_params.directory)
+copy2('seq2seq/eval_sent_embeddings_labels_in_expl.py', eval_params.directory)
 
 #import sys
 #sys.path.insert(0, eval_params.directory)
